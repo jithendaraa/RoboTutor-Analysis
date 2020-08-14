7712c6526cd107454483b02ebce643a6fbc3b29f
Jithendaraa Subramanian <jithen.subra@gmail.com>
2020-08-14 18:46:56 +0530

    Add function to calculate RMSE for ItemBKT

--- student_models/item_bkt.py
+++ student_models/item_bkt.py
@@ -1,4 +1,7 @@
 import numpy as np
+import sys
+sys.path.append('..')
+from helper import *
 
 # ITEM BKT is a binary BKT Model
 class ItemBKT:
@@ -68,16 +71,14 @@ class ItemBKT:
         for i in range(num_rows):
             student_num = student_nums[i]
             student_id = self.students[student_num]
-
             for skill_num in skill_nums[i]:
                 self.update_per_obs(corrects[i], student_num, skill_num)
-            
             self.learning_progress[student_id].append(self.know[student_num].copy())
 
-    def predict_p_correct(self, student_id, skills, update=False):
-        i = student_id
+    def predict_p_correct(self, student_num, skill, update=False):
+        i = student_num
         correct = 1.0
-        for skill in skills:
+        for j in skill:
             j = skill
             p_know = self.know[i][j]
             p_not_know = 1.0 - p_know
@@ -89,8 +90,24 @@ class ItemBKT:
             elif self.update_type == 'blame_weakest':
                 correct = min(correct, (p_know * p_not_slip) + (p_not_know * p_guess))
         
+        student_repsonse = None
         if update == True:
             student_repsonse = np.random.binomial(n=1, p=correct)
-            self.update([student_id], [skills], [student_repsonse])
+            self.update([student_num], [skill], [student_repsonse])
 
-        return correct
+        return correct, student_response
+
+    def get_rmse(self, student_nums, skills, observations):
+
+        predicted_responses  = []
+        predicted_p_corrects = []
+
+        for student_num in student_nums:
+            for skill in skills:
+                predicted_p_correct, predicted_response = predict_p_correct(student_num, skill, update=True)
+                predicted_p_corrects.append(predicted_p_correct)
+                predicted_responses.append(predicted_response)
+
+        prob_rmse       = rmse(predicted_p_corrects, observations)
+        sampled_rmse    = rmse(predicted_responses, observations)
+        return prob_rmse, sampled_rmse
