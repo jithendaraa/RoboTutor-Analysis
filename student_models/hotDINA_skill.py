78966e39ba701f84e042113af0ceea07d87c61f6
Jithendaraa Subramanian <jithen.subra@gmail.com>
2020-08-15 03:59:40 +0530

    get_rmse() added to hotDINA_skill with train_ratio

--- student_models/hotDINA_skill.py
+++ student_models/hotDINA_skill.py
@@ -1,49 +1,50 @@
+from helper import *
+import os
+import math
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sys
 sys.path.append('..')
-import matplotlib.pyplot as plt
-import math
-import os
-from helper import *
+
 
 class hotDINA_skill():
-    def __init__(self, params_dict, path_to_Qmatrix):
-        
+    def __init__(self, params_dict, path_to_Qmatrix, responsibilty='independent'):
+
         self.I = len(params_dict['theta'])
-        self.K = len(params_dict['a'])        
+        self.K = len(params_dict['a'])
         self.Q = pd.read_csv(path_to_Qmatrix, header=None).to_numpy()
-        
+
         # Skills for hotDINA_params. Theta Ix1 vector, the rest are Kx1
-        self.theta  = params_dict['theta']
-        self.a      = params_dict['a']
-        self.b      = params_dict['b']
-        self.learn  = params_dict['learn']
-        self.g      = params_dict['g']
-        self.ss     = params_dict['ss']
+        self.theta = params_dict['theta']
+        self.a = params_dict['a']
+        self.b = params_dict['b']
+        self.learn = params_dict['learn']
+        self.g = params_dict['g']
+        self.ss = params_dict['ss']
 
         self.bayesian_update = True
+        self.responsibilty = responsibilty
         # P(Know)'s of every student for every skill after every opportunity
-        self.knows  = {}
+        self.knows = {}
         self.avg_knows = {}
         for i in range(self.I):
             self.knows[i] = []
             self.avg_knows[i] = []
-
-        self.knews  = np.zeros((self.I, self.K))
-
+        self.knews = np.zeros((self.I, self.K))
         # Insert "knews" as knows@t=0
         for i in range(self.I):
             for k in range(self.K):
-                self.knews[i][k]    = sigmoid(1.7 * self.a[k] * (self.theta[i] - self.b[k]))
-            
+                self.knews[i][k] = sigmoid(
+                    1.7 * self.a[k] * (self.theta[i] - self.b[k]))
+
             self.knows[i].append(self.knews[i].tolist())
             self.avg_knows[i].append(np.mean(self.knews[i]))
 
     def update_skill(self, i, k, t, y, know):
         prior_know = know[k]
         posterior_know = None
-        p_correct = (self.ss[k] * prior_know) + (self.g[k] * (1 - prior_know)) 
+        p_correct = (self.ss[k] * prior_know) + (self.g[k] * (1 - prior_know))
         p_wrong = 1.0 - p_correct
 
         if self.bayesian_update:
@@ -51,21 +52,18 @@ class hotDINA_skill():
                 posterior_know = self.ss[k] * prior_know/p_correct
             elif y == 0:
                 posterior_know = (1 - self.ss[k]) * prior_know / p_wrong
-        
-            posterior_know = posterior_know + (1-posterior_know) * self.learn[k]    
-        
+
+            posterior_know = posterior_know + \
+                (1-posterior_know) * self.learn[k]
+
         else:
             posterior_know = prior_know + (1-prior_know) * self.learn[k]
-        
-        # print(posterior_know)
 
         know[k] = posterior_know
         return know
 
     def update(self, observations, items, users, bayesian_update=True, plot=True):
-        
         self.bayesian_update = bayesian_update
-
         for i in range(len(observations)):
             user = users[i]
             correct = int(observations[i])
@@ -76,64 +74,114 @@ class hotDINA_skill():
                 skill = skills[k]
                 if skill == 1:
                     know = self.update_skill(user, k, i, correct, know)
-            
             self.knows[user].append(know)
             self.avg_knows[user].append(np.mean(know))
-
         if plot:
             for i in range(self.I):
                 y = self.avg_knows[i]
                 x = np.arange(len(y)).tolist()
                 if bayesian_update:
-                    plt.plot(x, y, label="student_" + str(i) + "_bayesian" )
+                    plt.plot(x, y, label="student_" + str(i) + "_bayesian")
                 else:
-                    plt.plot(x, y, label="student_" + str(i) + "_no_bayesian" )
+                    plt.plot(x, y, label="student_" + str(i) + "_no_bayesian")
+            plt.legend()
             plt.show()
 
     def predict_response(self, item, user, update=False, bayesian_update=True, plot=False):
-        
         current_know = self.knows[user][-1]
         skills = self.Q[item]
         p_correct = 1.0
         p_min_correct = 1.0
-
         for k in range(len(skills)):
             skill = skills[k]
             if skill == 1:
-                p_correct_skill = (current_know[k] * self.ss[k]) + ((1 - current_know[k]) * self.g[k])
+                p_correct_skill = (
+                    current_know[k] * self.ss[k]) + ((1 - current_know[k]) * self.g[k])
                 p_correct = p_correct * p_correct_skill
                 p_min_correct = min(p_min_correct, p_correct_skill)
+        correct_response = int(np.random.binomial(n=1, p=p_correct))
+        min_correct_response = int(np.random.binomial(n=1, p=p_min_correct))
         
-        # response = Bern(p_correct)
-        correct_response        = int(np.random.binomial(n=1, p=p_correct))
-        min_correct_response    = int(np.random.binomial(n=1, p=p_min_correct))
-
         if update:
-            self.update([predicted_response], [item], [user], bayesian_update, plot)
-        
+            if self.responsibilty == 'independent':
+                self.update([correct_response], [item], [user], bayesian_update, plot)
+            elif self.responsibilty == 'blame_weakest':
+                self.update([min_correct_response], [item], [user], bayesian_update, plot)
+
         return correct_response, min_correct_response
-    
+
     def predict_responses(self, items, users, bayesian_update=True, plot=False, observations=None):
-        
+
         predicted_responses = []
 
         for i in range(len(users)):
             user = users[i]
             item = items[i]
-            predicted_response = self.predict_response(item, user, update=True, bayesian_update=bayesian_update, plot=plot)
+            predicted_response = self.predict_response(
+                item, user, update=True, bayesian_update=bayesian_update, plot=plot)
             predicted_responses.append(predicted_response)
-        
-        if observations != None:
-            accuracy = int(np.sum(np.abs(np.array(observations) - np.array(predicted_responses)))) * 100 /len(observations)
-            print("Accuracy: ", accuracy, "%")
 
+        if observations != None:
+            accuracy = int(np.sum(np.abs(np.array(
+                observations) - np.array(predicted_responses)))) * 100 / len(observations)
+            print("Accuracy: ", accuracy, "%")
         return predicted_responses
+
+    def get_rmse(self, users, items, observations, train_ratio=0.5):
+
+        predicted_responses = []
+        idxs = []
+        corrects = []
+        unique_users = pd.unique(np.array(users)).tolist()
+
+        for i in range(len(users)):
+            user = users[i]
+            if i > 0 and user != users[i-1]:
+                idxs.append(i)
+        
+        for i in range(len(idxs)):
+            if i == 0:
+                items_ = items[0:idxs[i]]
+                users_ = users[0:idxs[i]]
+                obs_ = observations[0:idxs[i]]
+
+            else:
+                items_ = items[idxs[i-1]:idxs[i]]
+                users_ = users[idxs[i-1]:idxs[i]]
+                obs_ = observations[idxs[i-1]:idxs[i]]
+
+            entries = len(items_)
+            test_idx = int(train_ratio * entries)
+            # Update on training data
+            self.update(obs_[:test_idx], items_[:test_idx], users_[:test_idx], plot=False)
+            # Test accuracy on test data
+            _users = users_[test_idx:]
+            _items = items_[test_idx:]
+            _obs = obs_[test_idx:]
+
+            if len(corrects) == 0:
+                corrects = _obs.copy()
+            else:
+                corrects = corrects + _obs
+
+            for j in range(len(_users)):
+                user = _users[j]
+                item = _items[j]
+                correct_response, min_correct_response = self.predict_response(item, user, update=True)
+                if self.responsibilty == 'independent':
+                    predicted_responses.append(correct_response)
+                elif self.responsibilty == 'blame_weakest':
+                    predicted_responses.append(min_correct_response)
+
+        rmse_val = rmse(predicted_responses, corrects)
+        return rmse_val
 
     def plot_avg_knows(self):
         for i in range(self.I):
             y = self.avg_knows[i]
             x = np.arange(len(y)).tolist()
             plt.plot(x, y, label="student" + str(i))
+
 
 if __name__ == '__main__':
 
@@ -147,13 +195,15 @@ if __name__ == '__main__':
 
     path_to_Qmatrix = os.getcwd() + '/../../hotDINA/qmatrix.txt'
 
-    path_to_data_file = os.getcwd() + '/../../hotDINA/pickles/data/data'+ village + '_' + observations +'.pickle'
+    path_to_data_file = os.getcwd() + '/../../hotDINA/pickles/data/data' + \
+        village + '_' + observations + '.pickle'
     data_file = Path(path_to_data_file)
 
     if data_file.is_file() == False:
         # if data_file does not exist, get it
         os.chdir('../../hotDINA')
-        get_data_file_command = 'python get_data_for_village_n.py -v ' + village + ' -o ' + observations 
+        get_data_file_command = 'python get_data_for_village_n.py -v ' + \
+            village + ' -o ' + observations
         os.system(get_data_file_command)
         os.chdir('../RoboTutor-Analysis/student_models')
 
@@ -161,7 +211,7 @@ if __name__ == '__main__':
     with open(path_to_data_file, 'rb') as handle:
         data_dict = pickle.load(handle)
     os.chdir('../RoboTutor-Analysis/student_models')
-    
+
     # update(self, observations, items, users, bayesian_update=True, plot=True)
 
     observations = data_dict['y']
