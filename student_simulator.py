4c8a619dd7f9b87c56eb7317e2a7e0ab55c8d081
Jithendaraa Subramanian <jithen.subra@gmail.com>
2020-08-18 02:04:50 +0530

    StudentSimulator now supports all 4 models fully

--- student_simulator.py
+++ student_simulator.py
@@ -17,7 +17,7 @@ from student_models.hotDINA_skill import hotDINA_skill
 from student_models.hotDINA_full import hotDINA_full
 
 class StudentSimulator():
-    def __init__(self, village="114", observations="10000", student_model_name="ItemBKT", subscript="student_specific", matrix_type='math'):
+    def __init__(self, village="114", observations="10000", student_model_name="ItemBKT", subscript="student_specific"):
         self.CONSTANTS = {
             "PATH_TO_CTA"                           : "Data/CTA.xlsx",
             "PATH_TO_ACTIVITY_TABLE"                : "Data/Activity_table_v4.1_22Apr2020.pkl",
@@ -39,7 +39,7 @@ class StudentSimulator():
         self.hotDINA_full_slurm_files = {}
         self.params_dict = {}
         self.set_village_paths()
-        self.set_data(matrix_type)
+        self.set_data()
         self.set_slurm_files()
         self.set_params_dict()
         self.set_uniq_activities()
@@ -51,7 +51,7 @@ class StudentSimulator():
         file = "Data/village_" + village + "/village_" + village + "_step_transac.txt"
         self.CONSTANTS["PATH_TO_VILLAGE_STEP_TRANSAC_FILES"].append(file)
     
-    def set_data(self, matrix_type='math'):
+    def set_data(self):
 
         self.cta_df = read_cta_table(self.CONSTANTS["PATH_TO_CTA"])
         self.kc_list = self.cta_df.columns.tolist()
@@ -69,6 +69,7 @@ class StudentSimulator():
             self.uniq_student_ids.append("new_student")
         else:
             self.uniq_student_ids, self.student_id_to_village_map = get_uniq_transac_student_ids(self.CONSTANTS["PATH_TO_VILLAGE_STEP_TRANSAC_FILES"], [self.CONSTANTS["VILLAGE"]])
+            self.uniq_student_ids.append("new_student")
             self.student_id_to_village_map['new_student'] = [int(self.CONSTANTS['VILLAGE'])]
         
         self.kc_list_spaceless = remove_spaces(self.kc_list.copy())
@@ -136,10 +137,14 @@ class StudentSimulator():
         elif self.student_model_name == "hotDINA_skill":
             path = os.getcwd() + "/slurm_outputs"
             self.params_dict = slurm_output_params(path, village, self.hotDINA_skill_slurm_files[village])
+            # student proficiency for 'new_student'
+            self.params_dict['theta'].append(-np.log(9)/1.7)
         
         elif self.student_model_name == 'hotDINA_full':
             path = os.getcwd() + "/slurm_outputs"
             self.params_dict = slurm_output_params(path, village, self.hotDINA_full_slurm_files[village])
+            # student proficiency for 'new_student'
+            self.params_dict['theta'].append(-np.log(9)/1.7)
     
     def reset(self):
         student_model = self.student_model
@@ -148,31 +153,22 @@ class StudentSimulator():
             student_model.know_act          = self.checkpoint_know_act.copy()
             student_model.learning_progress = self.checkpoint_learning_progress.copy()
 
-        elif self.student_model_name == 'hotDINA_skill':
-            student_model.knows     = self.checkpoint_knows.copy()
-            student_model.knews     = self.checkpoint_knews.copy()
-            student_model.avg_knows = self.checkpoint_avg_knows.copy()
-        
-        elif self.student_model_name == 'hotDINA_full':
+        elif self.student_model_name == 'hotDINA_skill' or self.student_model_name == 'hotDINA_full':
             student_model.alpha     = self.checkpoint_know.copy()
             student_model.avg_knows = self.checkpoint_avg_knows.copy()
-
+        
     def checkpoint(self):
         student_model = self.student_model
+        
         if self.student_model_name == 'ActivityBKT':
             self.checkpoint_know                = student_model.know.copy()
             self.checkpoint_know_act            = student_model.know_act.copy()
             self.checkpoint_learning_progress   = student_model.learning_progress.copy()
         
-        elif self.student_model_name == 'hotDINA_skill':
-            self.checkpoint_knows               = student_model.knows.copy()
-            self.checkpoint_knews               = student_model.knews.copy()
-            self.checkpoint_avg_knows           = student_model.avg_knows.copy()
-        
-        elif self.student_model_name == 'hotDINA_full':
+        elif self.student_model_name == 'hotDINA_skill' or self.student_model_name == 'hotDINA_full':
             self.checkpoint_know                = student_model.alpha.copy()
             self.checkpoint_avg_knows           = student_model.avg_knows.copy()
-
+        
     def update_on_log_data(self, data_dict, train_split=1.0, train_students=None, bayesian_update=True, plot=True):
         """
             Description: Takes a train_split (float) and train_students (list). If train_size is 1, trains on all rows of activity table, else trains on (1 - train_size)*rows of activity_df.
@@ -189,10 +185,10 @@ class StudentSimulator():
             self.activity_bkt_update(data_dict)
         
         elif self.student_model_name == 'hotDINA_skill':
-            self.hotDINA_skill_update(data_dict, bayesian_update, plot)
+            self.hotDINA_skill_update(data_dict, bayesian_update, plot, train_students=train_students)
         
         elif self.student_model_name == 'hotDINA_full':
-            self.hotDINA_full_update(data_dict)
+            self.hotDINA_full_update(data_dict, train_students)
 
     def item_bkt_update(self, train_split, train_students):
         
@@ -234,7 +230,7 @@ class StudentSimulator():
         activities              = data_dict['activity_names']                
         self.student_model.update(activity_observations, student_nums, activities)
 
-    def hotDINA_skill_update(self, data_dict, bayesian_update=True, plot=True):
+    def hotDINA_skill_update(self, data_dict, bayesian_update=True, plot=True, train_students=None):
         if self.CONSTANTS["PRINT_PARAMS_FOR_STUDENT"] == True and self.student_model_name == 'hotDINA_skill':
             print("theta: ", self.params_dict['theta'])
             print('a: ', self.params_dict['a'])
@@ -247,21 +243,13 @@ class StudentSimulator():
         observations = data_dict['y']
         items = data_dict['items']
         users = data_dict['users']
-        self.student_model.update(observations, items, users, bayesian_update, plot)
+        self.student_model.update(observations, items, users, bayesian_update, plot, train_students)
     
-    def hotDINA_full_update(self, data_dict):
-        if self.CONSTANTS["PRINT_PARAMS_FOR_STUDENT"] == True and self.student_model_name == 'hotDINA_skill':
-            print("theta: ", self.params_dict['theta'])
-            print('a: ', self.params_dict['a'])
-            print('b: ', self.params_dict['b'])
-            print("learn: ", self.params_dict['learn'])
-            print("guess: ", self.params_dict['g'])
-            print("ss: ", self.params_dict['ss'])
-            print()
+    def hotDINA_full_update(self, data_dict, train_students):
         observations = data_dict['y']
         items = data_dict['items']
         users = data_dict['users']
-        self.student_model.update(observations, items, users)
+        self.student_model.update(observations, items, users, train_students)
 
 def check_ItemBKT(village, observations, student_simulator, train_ratio=0.75):
     path_to_step_transac_data   = 'Data/village_' + village + '/village_' + village + '_step_transac.txt'
@@ -278,7 +266,7 @@ def check_ItemBKT(village, observations, student_simulator, train_ratio=0.75):
     print("Majority Class RMSE:", majority_class_rmse)
 
 # FIXME 
-def check_ActivityBKT(village, observations, student_simulator, matrix_type='all', student_id=None, train_size=1.0):
+def check_ActivityBKT(village, observations, student_simulator, student_id=None, train_size=1.0):
     activity_df = student_simulator.activity_df
     student_1_act_df = activity_df
     if student_id != None:
