07b44f97aa56ade87e88d87ef8483ad3bbcea288
Jithendaraa Subramanian <jithen.subra@gmail.com>
2020-08-31 19:10:21 +0530

    Save to "plots/Training plots" instead of "plots"

--- RL_agents/ppo_logs/plot_rewards.py
+++ RL_agents/ppo_logs/plot_rewards.py
@@ -14,7 +14,6 @@ def draw_graph():
         ax1.clear()
 
         for type_ in RL_types:
-            
             reward_file_name = "rewards_type" + str(type_) + ".txt"
             graph_data = open(reward_file_name, 'r').read()
             lines = graph_data.split('\n')
@@ -28,7 +27,6 @@ def draw_graph():
         
             ax1.plot(xs[:], ys[:], linewidth=2, alpha=0.5, label="RL agent policy: type" + str(type_), color=colors[type_ - 1])
         
-
         ax1.set_title("RL agent policy")
         ax1.set_xlabel('Epochs')
         ax1.set_ylabel('Avg P(Know) across all skills')
@@ -60,7 +58,7 @@ def draw_graph():
         ax2.set_ylabel('Avg P(Know) across all skills')
         ax2.legend()
 
-        plt.savefig('../../plots/training_results.png', bbox_inches='tight', )
+        plt.savefig('../../plots/Training plots/training_results.png', bbox_inches='tight', )
 
     ani = animation.FuncAnimation(fig, animate, interval=1000)
 
