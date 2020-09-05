8f44d2709f294d7cd1f492f991c1c53a92b0b564
Jithendaraa Subramanian <jithen.subra@gmail.com>
2020-09-06 02:36:53 +0530

    get transactions txt wget from google drive

---
+++ wget_txnTables.py
@@ -0,0 +1,15 @@
+import os
+txn_txt_google_drive_links = ['https://drive.google.com/file/d/1SeMa3mlNnJgYyebUvFWhd9TDTXcOGcut/view?usp=sharing']
+
+for i in range(len(txn_txt_google_drive_links)):
+    link = txn_txt_google_drive_links[i]
+    village_num = 114 + i
+    file_id = link.split('/')[-2:-1][0]
+    file_name = "village_" + str(village_num) + "_KCSubtests.txt"
+    print(file_id)
+    os.chdir('Data/village_' + str(village_num))
+    wget_command = "wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=" + file_id + "' -O " + file_name
+    os.system(wget_command)
+    os.chdir('../../')
+    print(wget_command)
+    
