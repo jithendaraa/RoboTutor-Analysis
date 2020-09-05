f54f2964ac64fbbc2e5a046414967b4253187bb3
Jithendaraa Subramanian <jithen.subra@gmail.com>
2020-09-06 02:42:38 +0530

    create dir if missing

--- wget_txnTables.py
+++ wget_txnTables.py
@@ -7,7 +7,10 @@ for i in range(len(txn_txt_google_drive_links)):
     file_id = link.split('/')[-2:-1][0]
     file_name = "village_" + str(village_num) + "_KCSubtests.txt"
     print(file_id)
-    os.chdir('Data/village_' + str(village_num))
+    os.chdir('Data')
+    if "village_" + str(village_num) not in os.listdir('.'):
+        os.system("mkdir village_" + str(village_num))
+    os.chdir('village_' + str(village_num))
     wget_command = "wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=" + file_id + "' -O " + file_name
     os.system(wget_command)
     os.chdir('../../')
